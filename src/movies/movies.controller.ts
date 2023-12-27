2a7ec5e193354ca12b9eaa28b8c44283bdd79669
hwan20c <hwan20c@gmail.com>
2023-12-27 17:32:52 +0900

    fetch update

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -36,9 +36,6 @@ export class MoviesController {
 
   @Patch(':id')
   Patch(@Param('id') movieId: string, @Body() updateData) {
-    return {
-      updatedMovie: movieId,
-      ...updateData,
-    };
+    return this.moviesService.update(movieId, updateData);
   }
 }
