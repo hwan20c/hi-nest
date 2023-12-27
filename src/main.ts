b6a614399ee9aec412fa405952f893c5b5b30297
hwan20c <hwan20c@gmail.com>
2023-12-27 17:52:57 +0900

    main.ts update

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
+      whitelist: true, // Validator에 도달하기 전에 금지
+      forbidNonWhitelisted: true, // 이상한갓을 보내면 리퀘스트 자체를 금지
+      transform: true, // get으로 보낸건 string이기 때문에 형태변환을 자동으로 하게끔 하는 것
     }),
   );
   await app.listen(3000);
